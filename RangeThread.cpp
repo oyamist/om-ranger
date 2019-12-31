e347777eb48dc7d95a71c9c40369c6f1b2dc361b
Karl Lew <karl@firepick.org>
2019-12-30 19:33:17 -0800

    no message

--- RangeThread.cpp
+++ RangeThread.cpp
@@ -4,6 +4,7 @@
 #endif 
 #include <Wire.h>
 #include "src/tinycircuits/VL53L0X.h"    // Time-of-Flight Distance sensor
+#include "src/omilli/Thread.h"
 #include <Wireling.h>   // For interfacing with Wirelings
 #include "Accel3Thread.h"
 #include "OLED042Thread.h"
@@ -30,9 +31,12 @@ void RangeThread::setup(uint8_t port, uint16_t msLoop) {
     distanceSensor.init();
     distanceSensor.setTimeout(500);
     distanceSensor.setMeasurementTimingBudget((msLoop-1)*1000);
+    distanceSensor.startContinuous(); // 19mA
+
     mode = MODE_IDLE;
 }
 #define SLOWFLASH 10
+#define FASTFLASH 5
 
 void RangeThread::sweepForward(uint16_t dist) {
     AxisState * px = &accelThread.xState;
@@ -80,39 +84,32 @@ void RangeThread::sweepForward(uint16_t dist) {
     }
 }
 
+#define STEP_DIFF 50
+
 void RangeThread::sweepStep(uint16_t dist) {
     AxisState * px = &accelThread.xState;
     AxisState * py = &accelThread.yState;
     AxisState * pz = &accelThread.zState;
+    int32_t diffDist = distSlow-distFast;
 
     uint32_t now = om::ticks();
     uint32_t cycleTicks = now - px->lastState;
     CRGB curLed = ledThread.leds[0];
-    uint8_t blue = mode == 0x66;
+    uint8_t blue = 0x66;
     uint16_t brightness = 255;
-    if (dist < 250) { // Very close
-        brightness = (loops % SLOWFLASH) < SLOWFLASH/2 ? 32 : 255;
+    if (diffDist > STEP_DIFF) { // Very close
+        brightness = (loops % FASTFLASH) < FASTFLASH/2 ? 32 : 255;
         curLed = CRGB(0xff,0,blue);
         lraThread.setEffect(DRV2605_STRONG_CLICK_30); 
-    } else if (dist < 400) {
-        curLed = CRGB(0xff,0,blue);                  
+    } else if (diffDist < -STEP_DIFF) {
+        brightness = (loops % SLOWFLASH) < SLOWFLASH/2 ? 32 : 255;
+        curLed = CRGB(0xff,0,blue);
         if (loops % 2 == 0) {
             lraThread.setEffect(DRV2605_STRONG_CLICK_30); 
         }
-    } else if (dist < 650) { // Somewhat close
-        curLed = CRGB(0xcc,0x33,blue); 
-        if (loops % 3 == 0) {
-            lraThread.setEffect(DRV2605_STRONG_CLICK_30); 
-        }
-    } else if (dist < 1000) {
-        brightness = (loops % SLOWFLASH) < SLOWFLASH/2 ? 32 : 255;
+    } else {
         curLed = CRGB(0,0xaa,blue);
-        if (loops % 5 == 0) {
-            lraThread.setEffect(DRV2605_STRONG_CLICK_30); 
-        }
-    } else if (dist < 2000) {
-        curLed = CRGB(0,0xaa,blue);
-        if (loops % 8 == 0) {
+        if (loops % 16 == 0) {
             lraThread.setEffect(DRV2605_STRONG_CLICK_30); 
         }
     }
@@ -138,14 +135,17 @@ void RangeThread::loop() {
         ledThread.leds[0] = CRGB(0,0,0); // no contact
         if (mode != MODE_IDLE) { 
             om::println("MODE_IDLE standing by...");
-            monitorThread.quiet(true);
+            monitor.quiet(true);
             distanceSensor.stopContinuous(); // 0.006mA
         }
         mode = MODE_IDLE;
     } else {
         if (mode == MODE_IDLE) {
             om::println("Motion detected, active...");
-            monitorThread.quiet(false);
+            monitor.quiet(false);
+    distanceSensor.init();
+    distanceSensor.setTimeout(500);
+    distanceSensor.setMeasurementTimingBudget((msLoop-1)*1000);
             distanceSensor.startContinuous(); // 19mA
         }
         mode = absval(py->valSlow) < absval(pz->valSlow) 
@@ -159,7 +159,6 @@ void RangeThread::loop() {
     uint16_t d = distanceSensor.readRangeContinuousMillimeters();
     distFast = d * DIST_FAST + (1-DIST_FAST) * distFast;
     distSlow = d * DIST_SLOW + (1-DIST_SLOW) * distSlow;
-    int32_t diffDist = distSlow-distFast;
     uint16_t dist = distFast;
     if (dist < minRange || maxRange < dist) {
         return;
@@ -173,8 +172,6 @@ void RangeThread::loop() {
         om::print(distFast);
         om::print(" distSlow");
         om::print(distSlow);
-        om::print(" diffDist");
-        om::print(diffDist);
         om::println();
     }
 
--- RangeThread.h
+++ RangeThread.h
@@ -22,7 +22,7 @@ typedef enum ModeType {
 } ModeType;
 
 #define DIST_FAST 0.5
-#define DIST_SLOW 0.85
+#define DIST_SLOW 0.15
 
 typedef class RangeThread : om::Thread {
 public:
